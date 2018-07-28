5df71085878711dedce9b735aff1879308af0fa6
Hamid Ebadi <hebadi@e1z1r6p17.42.us.org>
2018-07-28 15:16:42 -0700

    day04 ex00-06

---
+++ piscine/day04/ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hebadi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/28 15:14:01 by hebadi            #+#    #+#             */
+/*   Updated: 2018/07/28 15:14:06 by hebadi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	else
+		return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
