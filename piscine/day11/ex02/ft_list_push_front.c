33d84b771e098e51352e34c4be73dc687639e778
Hamid Ebadi <hebadi@e1z1r5p3.42.us.org>
2018-08-08 21:28:56 -0700

    hello

---
+++ piscine/day11/ex02/ft_list_push_front.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hebadi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/08 21:25:20 by hebadi            #+#    #+#             */
+/*   Updated: 2018/08/08 21:25:28 by hebadi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list *element;
+
+	if (*begin_list)
+	{
+		element = ft_create_elem(data);
+		element->next = *begin_list;
+		*begin_list = element;
+	}
+	else
+		*begin_list = ft_create_elem(data);
+}
